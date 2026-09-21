2f542201704907c94ce05e8b112ce45f96119aef
Swayam Sawant <swayamsawant741@gmail.com>
2026-09-21 12:19:53 +0530

    Time: 1 ms (99.57%), Space: 42.4 MB (89.83%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        long start = 1;
+        long end = x;
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            if(mid*mid > x){
+                end = mid - 1;
+            }
+            else if(mid* mid < x){
+                start = mid + 1;
+            }
+            else{
+                return (int) mid;
+            }
+        }
+        return (int) end;
+    }
+}
